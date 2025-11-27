d1f8b207d15b57b600752f8408cdad0bf4bb78c7
Hasan Ifthikar <Hsnifthi18@outlook.com>
2025-11-27 10:56:01 +0530

    testing that pages are moving correctly

---
+++ js/app.js
@@ -0,0 +1,34 @@
+fetch("components/nav/nav.html")
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById("nav").innerHTML = html;
+    })
+
+    fetch("components/home/home.html")
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById("home").innerHTML = html;
+    })
+
+function todo() {
+ 
+    const homepage = document.getElementById('home');
+    if (homepage) homepage.remove();
+    fetch("components/todo/todo.html")
+        
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("root").innerHTML = html;
+        })
+}
+
+function view() {
+  
+    const homepage = document.getElementById('home');
+    if (homepage) homepage.remove();
+    fetch("components/view/view.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("root").innerHTML = html;
+        })
+}
