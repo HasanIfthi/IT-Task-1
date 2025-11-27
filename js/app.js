f343203f3795ff56242b7c3130006b7e17327444
Hasan Ifthikar <Hsnifthi18@outlook.com>
2025-11-27 11:32:56 +0530

    adding functionalities to task adding page.

--- js/app.js
+++ js/app.js
@@ -4,18 +4,18 @@ fetch("components/nav/nav.html")
         document.getElementById("nav").innerHTML = html;
     })
 
-    fetch("components/home/home.html")
+fetch("components/home/home.html")
     .then(response => response.text())
     .then(html => {
         document.getElementById("home").innerHTML = html;
     })
 
 function todo() {
- 
+
     const homepage = document.getElementById('home');
     if (homepage) homepage.remove();
     fetch("components/todo/todo.html")
-        
+
         .then(response => response.text())
         .then(html => {
             document.getElementById("root").innerHTML = html;
@@ -23,12 +23,23 @@ function todo() {
 }
 
 function view() {
-  
+
     const homepage = document.getElementById('home');
     if (homepage) homepage.remove();
     fetch("components/view/view.html")
         .then(response => response.text())
         .then(html => {
             document.getElementById("root").innerHTML = html;
+        })
+}
+
+function logoClick() {
+    const homepage = document.getElementById('home');
+    if (homepage) homepage.remove();
+    fetch("components/home/home.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("root").innerHTML = html;
+
         })
 }
